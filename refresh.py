65d4db39dc273af23fbf52e27d7d1b832ac505aa
odulaja philip <odulajaphilip@gmail.com>
2022-09-14 18:49:35 +0100

    uses of static method

--- refresh.py
+++ refresh.py
@@ -11,7 +11,13 @@ class ClassTest:
     @classmethod
     def class_method(cls):
         print(f"Called class_method of {cls}")
+        
+    @staticmethod
+    def static_method():
+        print("Called static_method")
 
 # test = ClassTest()
 # test.instance_method()
 ClassTest.class_method()
+ClassTest.static_method()
+
