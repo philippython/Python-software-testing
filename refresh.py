2b7bc0b362a725e266fad6cbb93a1d2380255424
odulaja philip <odulajaphilip@gmail.com>
2022-09-14 16:27:12 +0100

    instance , class, static method

--- refresh.py
+++ refresh.py
@@ -1,6 +1,17 @@
 #  destructing variables
-t = [3, 5, 6,]
-x,_, y = t 
+t = [3, 5,6, 7, 2, 6]
 
+*_, x, y = t 
 
-print(x, y)
+class ClassTest:
+    
+    def instance_method(self):
+        print(f"Called instance_method of {self}")
+
+    @classmethod
+    def class_method(cls):
+        print(f"Called class_method of {cls}")
+
+# test = ClassTest()
+# test.instance_method()
+ClassTest.class_method()
