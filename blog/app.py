c06ec42e8be266da36bc3f022bb96ae0f636ed63
odulaja philip <odulajaphilip@gmail.com>
2022-09-18 23:27:24 +0100

    Created read, creat , list fumctions to apps.py

--- blog/app.py
+++ blog/app.py
@@ -5,8 +5,26 @@ def menu():
 
     print_blogs()
     selection = input(MENU_PROMPT)
+    while selection != 'q':
+        if selection == 'c':
+            ask_to_create_blog()
+        if selection == 'l':
+            ask_to_list_blog()
+        if selection == 'r':
+            ask_to_read_blog()
+        if selection == 'p':
+            ask_to_create_post()
+        selection = input(MENU_PROMPT)
 
 def print_blogs():
     for blogname , blog in blogs.items():
         print('- {}'.format(blog))
 
+def ask_to_create_blog():
+    pass
+def ask_to_list_blog():
+    pass
+def ask_to_read_blog():
+    pass
+def ask_to_create_post():
+    pass
