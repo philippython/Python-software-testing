6f23315efec3d7319c71f360cebbbd8717ee754c
odulaja philip <odulajaphilip@gmail.com>
2022-09-20 19:33:52 +0100

    @Abstract classes and methods

--- refresh.py
+++ refresh.py
@@ -1,6 +1,6 @@
 #  destructing variables
 from unicodedata import name
-
+from abc import ABC , classmethod
 
 t = [3, 5,6, 7, 2, 6]
 
@@ -18,6 +18,8 @@ class ClassTest:
     @staticmethod
     def static_method():
         print("Called static_method")
+
+
 
 # test = ClassTest()
 # test.instance_method()
@@ -39,3 +41,50 @@ class Book:
 book = Book("A thousand years", "hardcover", 450)
 print(book)
 
+# Python program showing
+# abstract base class work
+
+from abc import ABC, abstractmethod
+
+class Polygon(ABC):
+
+	@abstractmethod
+	def noofsides(self):
+		pass
+
+class Triangle(Polygon):
+
+	# overriding abstract method
+	def noofsides(self):
+		print("I have 3 sides")
+
+class Pentagon(Polygon):
+
+	# overriding abstract method
+	def noofsides(self):
+		print("I have 5 sides")
+
+class Hexagon(Polygon):
+
+	# overriding abstract method
+	def noofsides(self):
+		print("I have 6 sides")
+
+class Quadrilateral(Polygon):
+
+	# overriding abstract method
+	def noofsides(self):
+		print("I have 4 sides")
+
+# Driver code
+R = Triangle()
+R.noofsides()
+
+K = Quadrilateral()
+K.noofsides()
+
+R = Pentagon()
+R.noofsides()
+
+K = Hexagon()
+K.noofsides()
