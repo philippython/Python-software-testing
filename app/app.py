1a3ad9d909bcd8af037bfc19874d9fad1f6f1c28
odulaja philip <odulajaphilip@gmail.com>
2022-09-21 04:51:50 +0100

    Setup flask ap

--- app/app.py
+++ app/app.py
@@ -1,1 +1,11 @@
-ECHO is on.
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return jsonify({"message" : "HelloWorld"})
+
+
+if __name__ == "__main__":
+    app.run()
