a9217a54f159d47579f16706450bb1e6a572ae17
odulaja philip <odulajaphilip@gmail.com>
2022-09-22 06:43:37 +0100

    initialize models unit test

---
+++ starter_code/tests/unit/models/test_item.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+from models.item import ItemModel
+
+class TestItem(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_get_item(self):
+        pass
+
+    def test_put_item(self):
+        pass
+
+    def test_post_item(self):
+        pass
+    
+    def test_delete_item(self):
+        pass
