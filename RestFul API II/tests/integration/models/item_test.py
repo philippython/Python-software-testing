96d9cc0f7b2c4bd157af9a181779918124cdbd2c
odulaja philip <odulajaphilip@gmail.com>
2022-09-23 03:47:01 +0100

    pip install flask jwt

--- RestFul API II/tests/integration/models/item_test.py
+++ RestFul API II/tests/integration/models/item_test.py
@@ -5,7 +5,7 @@ from tests.base_test import BaseTest
 class ItemTest(BaseTest):
     def test_crud(self):
         with self.app_context():
-            item = ItemModel('test', 19.99)
+            item = ItemModel('test', 19.99, 1)
 
             self.assertIsNone(ItemModel.find_by_name('test'),
                               "Found an item with name {}, but expected not to.".format(item.name))
