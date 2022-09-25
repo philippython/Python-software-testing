92f9e45326aa1abcc0209b14d1c2b22da4f52b49
odulaja philip <odulajaphilip@gmail.com>
2022-09-25 17:32:32 +0100

    user system test

---
+++ RestFul API II/tests/system/test_user.py
@@ -0,0 +1,18 @@
+from models.user import UserModel
+from tests.base_test import BaseTest
+import unittest , json
+
+class UserTest(BaseTest):
+    
+    def test_register_user(self):
+        pass
+    
+    def test_login_user(self):
+        pass        
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
