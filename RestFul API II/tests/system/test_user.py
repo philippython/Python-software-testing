1dcf13f49402984520c5bcdb2adc5f66fdce196b
odulaja philip <odulajaphilip@gmail.com>
2022-09-26 03:53:57 +0100

    user system test

--- RestFul API II/tests/system/test_user.py
+++ RestFul API II/tests/system/test_user.py
@@ -11,15 +11,30 @@ class UserTest(BaseTest):
             with self.app_context() :
                 response = client.post('/register', data=json.dumps(data), headers=headers)
                 self.assertEqual(response.status_code, 201)
+                self.assertIsNotNone(UserModel.find_by_username('Test'))
                 self.assertDictEqual(json.loads(response.data), {"message": "User created successfully."})
 
-    def test_login_user(self):
-        pass        
+    # def test_register_and_login(self):
+    #     data = {'username' : 'Test','password' : 'Test1234'}
+    #     headers = { "content-Type": "application/json"}
+    #     with self.test_client() as client:
+    #         with self.app_context() :
+    #             client.post('/register', data=json.dumps(data), headers=headers)
+    #             auth_request = client.post('/auth', data=json.dumps(data), headers=headers)
 
+    #             self.assertDictEqual({ 'access_token' : 'rgghghsghhefrg' }, json.loads(auth_request.data))
+    #             self.assertIn('access_token', json.loads(auth_request.data).keys())
+    
     def test_register_duplicate_user(self):
-        pass
+        data = {'username' : 'Test','password' : 'Test1234'}
+        headers = { "content-Type": "application/json"}
+        with self.test_client() as client:
+            with self.app_context() :
+                client.post('/register', data=json.dumps(data), headers=headers)
+                response = client.post('/register', data=json.dumps(data), headers=headers)
 
-
+                self.assertDictEqual({"message": "A user with that username already exists"}, json.loads(response.data))
+                self.assertEqual(response.status_code, 400)
 
 
 if __name__ == "__main__":
