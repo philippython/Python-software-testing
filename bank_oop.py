24bd5ef707ea4c094371fd98945c058f87eb6d7f
odulaja philip <odulajaphilip@gmail.com>
2022-09-27 16:25:33 +0100

    Testing account classes

--- bank_oop.py
+++ bank_oop.py
@@ -1,12 +1,12 @@
 class Account:
 
-    def __init__(self, name: str, acc: str) -> None:
+    def __init__(self, name: str, account_num: int) -> None:
         self.name = name
-        self.acc = acc
+        self.account_num = account_num
         self.balance = 0.00
     
     def show_menu(self):
-        return 'Welcome back %s , Your Current balance #%s Acount Number : %s' % (self.name, self.balance, self.acc)
+        return 'Welcome back %s , Your Current balance #%s Acount Number : %s' % (self.name, self.balance, self.account_num)
 
     def get_balance(self):
         return 'Your Available Balance is #%s' % (self.balance)
@@ -21,11 +21,14 @@ class Account:
             return 'Withdrawal Successful\nYour Available Balance is #%s' % (self.balance)
         return 'Insufficient Funds\nYour Available Balance is #%s' % (self.balance)
 
-    def transfer(self, amount: float):
+    def transfer(self, account, amount: float):
         if self.balance > amount:
             self.balance -= amount
-            return 'Withdrawal Successful\nYour Available Balance is #%s' % (self.balance)
+            account.balance += amount
+            return 'Transfer Successfully made to %s \nYour Available Balance is #%s' % (account.name, self.balance)
         return 'Insufficient Funds\nYour Available Balance is #%s' % (self.balance)
 
-        
-        
+
+#   
+
+    
