2ea6b46139f595f1ed3ce8ff9886390762a0f97b
odulaja philip <odulajaphilip@gmail.com>
2022-10-02 21:17:38 +0100

    created library instnace method

---
+++ library.py
@@ -0,0 +1,19 @@
+
+class Library:
+
+    def __init__(self):
+        self.shelf = {}
+
+    def add_book(self, book_name, book_rating):
+        
+        if book_name not in self.shelf.keys():
+            self.shelf[book_name] = book_rating
+        else:
+            return f"Book {book_name} already in the library with rating {self.shelf.get(book_name)}"
+
+    def get_all_books(self):
+        return self.shelf
+
+book_library = Library()
+book_library.add_book('Journey of philip', 8.9)
+print(book_library.get_all_books())
