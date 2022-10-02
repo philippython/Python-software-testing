a5916bc90d6edc329650a90ad4af53e246f4557e
odulaja philip <odulajaphilip@gmail.com>
2022-10-02 21:23:58 +0100

    added get all books method

--- library.py
+++ library.py
@@ -11,9 +11,16 @@ class Library:
         else:
             return f"Book {book_name} already in the library with rating {self.shelf.get(book_name)}"
 
+    def get_book(self, book_name):
+        if book_name not in self.shelf.keys():
+            return f"Book {book_name} does not exist in the library"
+        else:
+            return self.shelf.get(book_name)
+
     def get_all_books(self):
         return self.shelf
 
 book_library = Library()
 book_library.add_book('Journey of philip', 8.9)
+book_library.add_book('Rise', 9.9)
 print(book_library.get_all_books())
