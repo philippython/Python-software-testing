b25a3af1df4cb8a9f6d83cb5f70d68ce1ebcb52d
odulaja philip <odulajaphilip@gmail.com>
2022-10-04 12:03:13 +0100

    updated webdriver to version 106

--- section10/code/tests/acceptance/navigation.feature
+++ section10/code/tests/acceptance/navigation.feature
@@ -1,6 +1,6 @@
 Feature: Test navigation pages 
 
-Scenario: Homepage can go to Blog
+  Scenario: Homepage can go to Blog
     Given  I am on the Homepage
     When I click on the link with id "blog-link"
     Then I am on the blog page
--- section10/code/tests/acceptance/steps/navigation.py
+++ section10/code/tests/acceptance/steps/navigation.py
@@ -1,1 +1,12 @@
 from behave import *
+from selenium import webdriver
+
+DRIVER_PATH = 'C:/Development/chromedriver'
+
+use_step_matcher('re')
+
+@given('I am on the Homepage')
+def step_impl(context):
+    browser = webdriver.Chrome(DRIVER_PATH)
+    browser.get('http://127.0.0.1:5000')  
+
