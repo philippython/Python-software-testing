975c2b1702721bdaaa39352d9145e6554d0d3501
odulaja philip <odulajaphilip@gmail.com>
2022-10-04 21:10:44 +0100

    used extended unpacking

--- section10/code/tests/acceptance/steps/content.py
+++ section10/code/tests/acceptance/steps/content.py
@@ -8,11 +8,11 @@ use_step_matcher('re')
 
 @then('There is a title shown on the page')
 def step_impl(context):
-    title = context.browser.find_element(HomePageLocators.TITLE[0], HomePageLocators.TITLE[1])
+    title = context.browser.find_element(*HomePageLocators.TITLE)
     assert title.is_displayed()
 
 
 @step('The title tag has content "(.*)"')
 def step_impl(context, content):
-    title =  context.browser.find_element(HomePageLocators.TITLE[0], HomePageLocators.TITLE[1])
+    title =  context.browser.find_element(*HomePageLocators.TITLE)
     assert title.text == content
