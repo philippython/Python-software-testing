2138594db2b833baff19337bc7a3fe57de22f3a1
odulaja philip <odulajaphilip@gmail.com>
2022-10-04 20:31:10 +0100

    Integrated HomePageLocator class

--- section10/code/tests/acceptance/steps/content.py
+++ section10/code/tests/acceptance/steps/content.py
@@ -1,7 +1,5 @@
 from behave import *
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
+from locators.home_page import HomePageLocators
 
 DRIVER_PATH = 'C:/Development/chromedriver'
 
@@ -10,11 +8,11 @@ use_step_matcher('re')
 
 @then('There is a title shown on the page')
 def step_impl(context):
-    title = context.browser.find_element(by=By.TAG_NAME, value='h1')
+    title = context.browser.find_element(HomePageLocators.TITLE[0], HomePageLocators.TITLE[1])
     assert title.is_displayed()
 
 
 @step('The title tag has content "(.*)"')
 def step_impl(context, content):
-    title =  context.browser.find_element(by=By.TAG_NAME, value='h1')
+    title =  context.browser.find_element(HomePageLocators.TITLE[0], HomePageLocators.TITLE[1])
     assert title.text == content
