17509469333f4f2dab993f5db403ade255246bc0
odulaja philip <odulajaphilip@gmail.com>
2022-10-04 12:44:43 +0100

    Scernario Passed

--- section10/code/tests/acceptance/steps/interactions.py
+++ section10/code/tests/acceptance/steps/interactions.py
@@ -1,9 +1,10 @@
 from behave import *
+from selenium.webdriver.common.by import By
+
 
 use_step_matcher('re')
 
-
-@when('I click on the link with id "blog-link"')
-def step_impl(context):
-    link = context.browser.find_elemet_id("blog-link")
+@when('I click on the link with id "(.*)"')
+def step_impl(context, link_id):
+    link = context.browser.find_element(by=By.ID , value=link_id)
     link.click()
