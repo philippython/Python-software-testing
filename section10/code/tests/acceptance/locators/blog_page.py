9021f5050781e422af34d9a829bcb1b9dba0592d
odulaja philip <odulajaphilip@gmail.com>
2022-10-05 14:20:04 +0100

    created blog page models

---
+++ section10/code/tests/acceptance/locators/blog_page.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+
+class HomePageLocators:
+    ADD_POST_LINK = By.ID , "add-post-link"
+    POST_SECTION = By.ID, "post-link"
+    HOME_LINK = By.ID , "home-link"
--- section10/code/tests/acceptance/page_model/blog_page.py
+++ section10/code/tests/acceptance/page_model/blog_page.py
@@ -1,8 +1,12 @@
 from page_model.base_page import BasePage
+from locators.blog_page import BlogPageLocators
 
 class  BlogPage(BasePage):
 
+    @property
     def url(self):
         return super(BlogPage, self).url + '/blog'
 
-    
+    @property
+    def add_post_link(self):
+
