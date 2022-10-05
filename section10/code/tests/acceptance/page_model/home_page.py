855adc146a301b660a0c28dd17be27fabe84da51
odulaja philip <odulajaphilip@gmail.com>
2022-10-05 13:42:05 +0100

    testing base loactors and pbase_models

--- section10/code/tests/acceptance/page_model/home_page.py
+++ section10/code/tests/acceptance/page_model/home_page.py
@@ -1,4 +1,9 @@
-from locators.base_page import BasePageLocators
+from section10.code.tests.acceptance.page_model.base_page import BasePage
 
-class HomePage:
 
+from page_model.base_page import BasePage
+
+class HomePage(BasePage):
+
+    def url(self):
+        return super(HomePage, self).url + '/'
