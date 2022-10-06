9a9911404bfe2f5a3f2b705b7796be3c194a393d
odulaja philip <odulajaphilip@gmail.com>
2022-10-06 18:30:57 +0100

    property function vs @property decorator

--- inheritance.py
+++ inheritance.py
@@ -21,12 +21,31 @@ class Developer:
         self.level = level
         self.exp = exp
         self.name = name
+        self.networth = 0
+
+    @property
+    def worth(self):
+        return self.networth
+
+    @worth.setter
+    def worth(self, worth):
+        if worth > 75:
+            return "Worth is too high"
+        self.networth = worth
+
+
 
     @classmethod
     def senority_level(cls, name, exp):
         return cls(name, exp, "senior" if exp > 3 else 'junior')
 
-    
+    @staticmethod
+    def gender(name):
+        return "male" if name[0] in ('j', 'p', 'm') else "female"
+            
+        
 
-philip = Developer.senority_level('john', 8)
-print(philip.level)
+
+john = Developer.senority_level('john', 8)
+print(john.worth)
+
