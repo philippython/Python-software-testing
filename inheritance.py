d74d195a12349da08ec83e561534f50fa5624891
odulaja philip <odulajaphilip@gmail.com>
2022-10-06 17:03:12 +0100

    classmethod

--- inheritance.py
+++ inheritance.py
@@ -10,7 +10,20 @@ class Parent(ABC):
         print('This is a parent class')
 
 
-class Child(Parent):
-
+class Child(Parent): 
     def func1(self):
         print('This is a child class')
+
+
+class Developer:
+
+    def __init__(self, exp):
+        self.senority = self.senority_level()
+        self.exp = exp
+
+    @classmethod
+    def senortiy_level(cls, exp):
+        return "senior" if exp > 3 else 'junior' 
+
+philip = Developer(7)
+print(philip.exp)
