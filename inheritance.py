bcf6598023ed68864107767b7a7d1e86657c1e62
odulaja philip <odulajaphilip@gmail.com>
2022-10-06 16:48:14 +0100

    adstract class and methods

--- inheritance.py
+++ inheritance.py
@@ -1,1 +1,16 @@
-ECHO is on.
+from abc import ABC, abstractmethod
+
+class Parent(ABC):
+
+    @abstractmethod
+    def func1(self):
+        pass
+
+    def func2(self):
+        print('This is a parent class')
+
+
+class Child(Parent):
+
+    def func1(self):
+        print('This is a child class')
